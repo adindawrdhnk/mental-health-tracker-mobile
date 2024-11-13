9fb7b0c633ac261478e06ee1340fcc0cbfa175b5
adindawrdhnk <adwrdhnk@gmail.com>
2024-11-13 12:34:35 +0700

    selesai tutor 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/widgets/left_drawer.dart';
-import 'package:mental_health_tracker/screens/moodentry_form.dart'; 
-import 'package:mental_health_tracker/widgets/mood_card.dart';
+import 'package:mental_health_tracker/widgets/mood_card.dart'; // New import
+
 
 class MyHomePage extends StatelessWidget {
-  final String npm = '2306165856'; 
-  final String name = 'Adinda Maharani Wardhana'; 
-  final String className = 'PBP A'; 
+  final String npm = '2306165856'; // NPM
+  final String name = 'Adinda Maharani Wardhana'; // Nama
+  final String className = 'PBP C'; // Kelas
 
   MyHomePage({super.key});
 
@@ -19,6 +19,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const LeftDrawer(),
       appBar: AppBar(
         iconTheme: const IconThemeData(color: Colors.white),
         title: const Text(
@@ -30,7 +31,6 @@ class MyHomePage extends StatelessWidget {
         ),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      drawer: const LeftDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -105,63 +105,4 @@ class InfoCard extends StatelessWidget {
       ),
     );
   }
-}
-
-class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
-
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).colorScheme.secondary,
-      borderRadius: BorderRadius.circular(12),
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-
-          if (item.name == "Tambah Mood") {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => MoodEntryFormPage(),
-              ),
-            );
-          }
-        },
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ItemHomepage {
-  final String name;
-  final IconData icon;
-
-  ItemHomepage(this.name, this.icon);
 }
